a9888770b3a5c31a1feda61af13731088207b478
Ramoun <omartarekramoun@gmail.com>
2020-08-15 08:47:07 -0400

    adding some modifications for enhancing the rearrange() function

--- computer/software/tools/programming_languages/general_purpose/python/the_lingo/mini_projects/corey_schafer/automate_parsing_and_renaming_of_multiple_files/automate_parsing_and_renaming_of_multiple_files.py
+++ computer/software/tools/programming_languages/general_purpose/python/the_lingo/mini_projects/corey_schafer/automate_parsing_and_renaming_of_multiple_files/automate_parsing_and_renaming_of_multiple_files.py
@@ -23,21 +23,26 @@ def rearrange():
     for file in os.listdir():
         # splitting the extension
         f_name_and_ext = os.path.splitext(file)
-        f_name_no_ext = f_name_and_ext[0].split('-')
+        ext = f_name_and_ext[1]
+        f_name = f_name_and_ext[0]
+
+
+        # splitting the file name into parts
+        f_name_parted = f_name.split('-')
+
         # adding space in front of the course name
-        f_name_no_ext[0] = ' ' + f_name_no_ext[0]
+        f_name_parted[0] = ' ' + f_name_parted[0]
 
         # moving number to the front with zfill and adding spaces
-        number = f_name_no_ext.pop(1)
+        number = f_name_parted.pop(1)
         number = number.strip().zfill(2)
         number += ' '
-        f_name_no_ext.insert(0, number)
+        f_name_parted.insert(0, number)
 
         # finally joining everything back together
-        final_no_ext = '-'.join(f_name_no_ext)
-        final_with_ext = final_no_ext + f_name_and_ext[1]
+        final_no_ext = '-'.join(f_name_parted)
+        final_with_ext = final_no_ext + ext
         print(final_with_ext)
-
     
 
 if __name__ == '__main__':
